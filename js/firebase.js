a99dcb46c93d409b8ddc6f289d202f0e0ffb5a29
Mac Clecio <cleciodev.mac@gmail.com>
2023-02-06 18:24:34 -0300

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDydnl98mLoRCMpM_Lqkr0XjA_0WmtOJtw",
+  authDomain: "projetofirebase-d17aa.firebaseapp.com",
+  projectId: "projetofirebase-d17aa",
+  storageBucket: "projetofirebase-d17aa.appspot.com",
+  messagingSenderId: "580351800865",
+  appId: "1:580351800865:web:8f5bfd0c16dbfb37b51560",
+  measurementId: "G-TT707TTF64"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
