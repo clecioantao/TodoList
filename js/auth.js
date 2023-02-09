8eef50133a3ff7405a549159aca978c2b6d34098
Mac Clecio <cleciodev.mac@gmail.com>
2023-02-09 19:31:34 -0300

    modulo 19

--- js/auth.js
+++ js/auth.js
@@ -1,35 +1,72 @@
-// funçao que trata submissao form autenticação
+
+// Traduz para português brasileiro a autenticação do Firebase
+firebase.auth().languageCode = 'pt-BR'
+
+// Função que trata a submissão do formulário de autenticação
 authForm.onsubmit = function (event) {
-  showItem(loading);
+  showItem(loading)
   event.preventDefault()
   if (authForm.submitAuthForm.innerHTML == 'Acessar') {
     firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
       console.log('Falha no acesso')
       console.log(error)
+      hideItem(loading)
     })
   } else {
     firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
       console.log('Falha no cadastro')
       console.log(error)
+      hideItem(loading)
     })
-
   }
 }
 
-// funçao que centraliza e trata autenticação
+// Função que centraliza e trata a autenticação
 firebase.auth().onAuthStateChanged(function (user) {
-    hideItem(loading);
-    if(user){
-        showUserContent(user);
-    } else {
-        showAuth();
-    }
+  hideItem(loading)
+  if (user) {
+    showUserContent(user)
+  } else {
+    showAuth()
+  }
 })
 
-// funçao que permite ao usuario sair da conta pessoal
-function signOut(){
+// Função que permite ao usuário sair da conta dele
+function signOut() {
   firebase.auth().signOut().catch(function (error) {
-  console.log("Falha ao sair")
-  console.log(error)
+    console.log('Falha ao sair da conta')
+    console.log(error)
   })
+}
+
+// Função que permite o usuário fazer a verificação do e-mail dele
+function sendEmailVerification() {
+  showItem(loading)
+  var user = firebase.auth().currentUser
+  user.sendEmailVerification(actionCodeSettings).then(function () {
+    alert('E-mail de verificação foi enviado para ' + user.email + '! Verifique a sua caixa de entrada')
+  }).catch(function (error) {
+    alert('Houve um erro ao enviar o e-mail de verificação')
+    console.log(error)
+  }).finally(function () {
+    hideItem(loading)
+  })
+}
+
+// Função que permite o usuário redefinir a senha dele
+function sendPasswordResetEmail() {
+  var email = prompt('Redefinir senha! Informe o seu endereço de e-mail.', authForm.email.value)
+  if (email) {
+    showItem(loading)
+    firebase.auth().sendPasswordResetEmail(email, actionCodeSettings).then(function () {
+      alert('E-mail de redefinição de senha foi enviado para ' + email + '.')
+    }).catch(function (error) {
+      alert('Houve um erro ao enviar e-mail de redefinição de senha!')
+      console.log('tetetate', error)
+    }).finally(function () {
+      hideItem(loading)
+    })
+  } else {
+    alert('É preciso preencher o campo de e-mail para redefinir a senha!')
+  }
 }
--- js/utils.js
+++ js/utils.js
@@ -3,29 +3,31 @@ var authForm = document.getElementById('authForm')
 var authFormTitle = document.getElementById('authFormTitle')
 var register = document.getElementById('register')
 var access = document.getElementById('access')
-
 var loading = document.getElementById('loading')
-
 var auth = document.getElementById('auth')
 var userContent = document.getElementById('userContent')
-
 var userEmail = document.getElementById('userEmail')
+var sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
+var emailVerified = document.getElementById('emailVerified')
+var passwordReset = document.getElementById('passwordReset')
 
 
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
   authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
   authFormTitle.innerHTML = 'Insira seus dados para se cadastrar'
-  hideItem(register)
-  showItem(access)
+  hideItem(register)  // esconder atalho para cadastrar conta
+  showItem(access) // mostrar atalho para acessar
+  hideItem(passwordReset) // esconder resetar senha
 }
 
 // Alterar o formulário de autenticação para o acesso de contas já existentes
 function toggleToAccess() {
   authForm.submitAuthForm.innerHTML = 'Acessar'
   authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
-  hideItem(access)
-  showItem(register)
+  hideItem(access) // // esconder atalho para acessar cadastrar conta
+  showItem(register) // mostrar atalho para registro
+  showItem(passwordReset) // mostrar refefinir senha
 }
 
 // Simplifica a exibição de elementos da página
@@ -40,6 +42,14 @@ function hideItem(element) {
 
 // Mostrar conteúdo para usuários autenticados
 function showUserContent(user) {
+  console.log(user)
+  if (user.emailVerified){
+    emailVerified.innerHTML = 'Email verificado'
+    hideItem(sendEmailVerificationDiv)
+  } else {
+    emailVerified.innerHTML = 'Email nao verificado'
+    showItem(sendEmailVerificationDiv)
+  }
   userEmail.innerHTML = user.email
   hideItem(auth)
   showItem(userContent)
@@ -51,4 +61,9 @@ function showAuth() {
   authForm.password.value = ''
   hideItem(userContent)
   showItem(auth)
+}
+
+// atributos extras de configuração de e-mail
+var actionCodeSetting = {
+  url: 'http://127.0.0.1:5500/index.html'
 }
